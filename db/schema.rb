92d2683f63d73663c5c8046162cd6b77eb697b4d
Femke Knottenbelt <femknotgithub@gmail.com>
2019-09-11 18:10:29 +0000

    Adding Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190825084335) do
+ActiveRecord::Schema.define(version: 20190911174920) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
