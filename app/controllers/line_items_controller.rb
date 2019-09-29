cf22bb9c8f4fae7a6ba106f26ddf739084ab0c5d
Femke Knottenbelt <femknotgithub@gmail.com>
2019-09-29 17:23:53 +0000

    Finish cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart,
+        notice: "#{product.title} was added to your cart" }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
