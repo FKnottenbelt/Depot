eab5a0c282fc148e686f194a362403c4f2403c51
Femke Knottenbelt <femknotgithub@gmail.com>
2019-10-26 11:07:22 +0000

    Add remove cart when empyt using Ajax

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -53,4 +53,14 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to store_index_url
   end
+
+  test "should destroy cart via ajax" do
+    post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    @cart = Cart.find(session[:cart_id])
+
+    assert_difference('Cart.count', -1) do
+      delete cart_url(@cart)
+    end
+
+  end
 end
