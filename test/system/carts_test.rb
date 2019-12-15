f63d2093cbf5a8949fb424b3a4c64494d896d3ac
Femke Knottenbelt <femknotgithub@gmail.com>
2019-12-15 10:50:55 +0000

    Add test for hiding and showing cart

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -6,4 +6,24 @@ class CartsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Cart"
   # end
+
+  test "add to cart button show cart" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+    assert_selector "#cart"
+  end
+
+  test "empty cart button hides cart" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+    assert_selector "#cart"
+
+    accept_alert 'Are you sure?' do
+     click_button('Empty cart')
+    end
+
+    assert_no_selector "#cart"
+  end
 end
